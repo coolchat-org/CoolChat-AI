9902ff0ed4e1a59c8cd0f8e857c46704ce3c26db
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2025-01-01 18:47:33 +0700

    Switch to pinecone. Not fix context yet.

--- app/api/services/chatService.py
+++ app/api/services/chatService.py
@@ -10,11 +10,14 @@ from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.chains.retrieval import create_retrieval_chain
+# from langchain.chains.qa_with_sources.retrieval import RetrievalQAWithSourcesChain
+from langchain_pinecone import PineconeVectorStore
 from app.core import db
 from app.core.config import settings
 from app.dtos.chatUserDto import LLMResponseDto
 from app.models.chatModel import ChatModel
 from app.models.models import PyObjectId
+from pinecone import Pinecone
 
 # BaseMessage[] to work with chat history
 chat_history: List[BaseMessage] = []
@@ -23,21 +26,29 @@ def response_from_LLM(index_path: str, query: str):
     retrieval_qa_chat_prompt: ChatPromptTemplate = pull("davidnguyen2212/retrievala-qa-chat-for-cool-chat")
     rephrase_prompt: ChatPromptTemplate = pull("langchain-ai/chat-langchain-rephrase")
 
-    model = ChatOpenAI(model="gpt-4o-mini", temperature=0.2, api_key=settings.OPENAI_API_KEY)
-    embedder = OpenAIEmbeddings(model="text-embedding-3-large", api_key=settings.OPENAI_API_KEY)
+    model = ChatOpenAI(model="gpt-4o-mini", temperature=0.2, api_key=settings.OPENAI_API_KEY, verbose=True)
+    embedder = OpenAIEmbeddings(model="text-embedding-3-small", api_key=settings.OPENAI_API_KEY)
     question_answer_chain = create_stuff_documents_chain(llm=model, prompt=retrieval_qa_chat_prompt)
 
-    vectorStore: FAISS = FAISS.load_local(folder_path=index_path, embeddings=embedder, allow_dangerous_deserialization=True)
-    retriever: VectorStoreRetriever = vectorStore.as_retriever()
+    # vectorStore: FAISS = FAISS.load_local(folder_path=index_path, embeddings=embedder, allow_dangerous_deserialization=True)
+    pc = Pinecone(api_key=settings.PINECONE_API_KEY)
+    index = pc.Index(host="https://org-index-8bb0cf6eb9b17d0f-oei5fcs.svc.aped-4627-b74a.pinecone.io")
+    vectorStore = PineconeVectorStore(index=index, embedding=embedder)
+    retriever = vectorStore.as_retriever()
     history_aware_retriever = create_history_aware_retriever(
         llm=model, retriever=retriever, prompt=rephrase_prompt
     )
 
-    ragChain = create_retrieval_chain(retriever=history_aware_retriever, combine_docs_chain=question_answer_chain)
+    ragChain = create_retrieval_chain(
+        retriever=history_aware_retriever, 
+        combine_docs_chain=question_answer_chain
+        )
     result = ragChain.invoke(input={"input": query, "chat_history": chat_history})
     if result:
         chat_history.append(HumanMessage(content=query))
         chat_history.append(AIMessage(result["answer"]))
+
+    print(result)
 
     return LLMResponseDto(
         reply=result["answer"],
